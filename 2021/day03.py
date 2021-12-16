2d2d59ed25acafa9486b593cd6b574ea96817a67
Joseph Rock <josephrock225@gmail.com>
2021-12-15 19:11:54 -0800

    working

--- 2021/day03.py
+++ 2021/day03.py
@@ -1,9 +1,6 @@
 import numpy as np
 from input_helper import str_list
 
-import sys
-np.set_printoptions(threshold=sys.maxsize)
-
 
 def get_array():
     in_list = str_list("3")
@@ -34,45 +31,48 @@ def pt_1(array):
 
 
 def pt_2(array):
-    oxy_a = np.transpose(array)
-    co2_a = oxy_a.copy()
+    a = np.transpose(array)
+    bit_pos = len(a)
 
-    bit_pos = len(oxy_a)
+    oxy_a = a.copy()
+    co2_a = a.copy()
 
+    # Oxygen Generator
     for i in range(bit_pos):
-        most_common = round(np.mean(oxy_a[i]))
-        indices = [index for index, bit in enumerate(oxy_a[i]) if bit != most_common]
+        rnd = np.mean(oxy_a[i])
+        if rnd >= .5:
+            keep_num = 1
+        else:
+            keep_num = 0
+
+        indices = [index for index, bit in enumerate(oxy_a[i]) if bit != keep_num]
         oxy_a = np.delete(oxy_a, indices, 1)
 
-        print()
-        print(i, most_common)
-        print()
-        print(oxy_a)
-
+    # CO2 Scrubber
     for i in range(bit_pos):
         if len(co2_a[i]) == 1:
             break
         else:
-            most_common = round(np.mean(co2_a[i]))
-            indices = [index for index, bit in enumerate(co2_a[i]) if bit == most_common]
+            rnd = np.mean(co2_a[i])
+            if rnd >= 0.5:
+                keep_num = 0
+            else:
+                keep_num = 1
+
+            indices = [index for index, bit in enumerate(co2_a[i]) if bit != keep_num]
             co2_a = np.delete(co2_a, indices, 1)
 
-    oxy_gen = int("".join(list(map(str, map(int, oxy_a)))))
-    co2_scrubber = int("".join(list(map(str, map(int, co2_a)))))
-
-    print(oxy_gen, binary2int(oxy_gen))
-    print(binary2int(co2_scrubber))
+    # TODO Make less gross
+    oxy_gen =      int("".join(list(map(str, oxy_a.flatten().tolist()))))
+    co2_scrubber = int("".join(list(map(str, co2_a.flatten().tolist()))))
 
     return binary2int(oxy_gen) * binary2int(co2_scrubber)
-
-
-
 
 
 def main():
     array = get_array()
     print("Part 1:", pt_1(array))
-    print(pt_2(array))
+    print("Part 2:", pt_2(array))
 
 
 main()

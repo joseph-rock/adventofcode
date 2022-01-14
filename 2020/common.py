82b0591cd0c24d7c53a8150b8ac003a62c850bf1
Joseph R <josephrock225@gmail.com>
2022-01-13 17:48:24 -0800

    renamed input_helper to common and added print function

--- 2020/common.py
+++ 2020/common.py
@@ -1,5 +1,5 @@
 
-def str_list(day_num: str) -> list[str]:
+def str_list(day_num: str) -> "list[str]":
     path = f"./input/2020day{day_num}.txt"
     with open(path) as f:
         str_list = f.read().splitlines()
@@ -7,7 +7,7 @@ def str_list(day_num: str) -> list[str]:
     return str_list
 
 
-def int_list(day_num: str) -> list[int]:
+def int_list(day_num: str) -> "list[int]":
     path = f"./input/2020day{day_num}.txt"
     with open(path) as f:
         int_list = list(map(int, f.readlines()))
@@ -21,3 +21,8 @@ def str_block(day_num: str) -> str:
         file = f.read()
 
     return file
+
+
+def print_ans(pt1="not finished", pt2="not finished"):
+    print(f"Part 1: {pt1}")
+    print(f"Part 2: {pt2}")

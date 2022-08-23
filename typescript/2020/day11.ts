e58f8431182a5d1ef7ef3f875fe7d7d0b1dc71a2
Joseph <josephrock225@gmail.com>
2022-08-23 13:37:36 -0700

    Refactor

--- typescript/2020/day11.ts
+++ typescript/2020/day11.ts
@@ -1,25 +1,51 @@
 import * as mod from "https://deno.land/std@0.103.0/collections/mod.ts";
 import { equal } from "https://deno.land/x/equal@v1.5.0/equal.ts";
 
-function seatingChart(): string[][] {
-  const input = Deno.readTextFileSync("../../input/2020/day11.txt");
-  return mod.chunked([...input].filter((c) => c != "\n"), input.indexOf("\n"));
+partOne();
+partTwo();
+
+function partOne(): void {
+  let beforeInput = seatingChart();
+
+  while (true) {
+    const afterInput = flipSeats(beforeInput, adjOccupiedSeats, 4);
+    if (equal(afterInput, beforeInput)) {
+      break;
+    }
+    beforeInput = cloneChart(afterInput);
+  }
+
+  console.log(totalOccupiedSeats(beforeInput));
 }
 
-function cloneChart(chart: string[][]): string[][] {
-  return [...chart].map((line) => [...line]);
+function partTwo(): void {
+  let beforeInput = seatingChart();
+
+  while (true) {
+    const afterInput = flipSeats(beforeInput, dirOccupiedSeats, 5);
+    if (equal(afterInput, beforeInput)) {
+      break;
+    }
+    beforeInput = cloneChart(afterInput);
+  }
+
+  console.log(totalOccupiedSeats(beforeInput));
 }
 
-function indexInRange(x: number, y: number, chart: string[][]): boolean {
-  return x >= 0 && x < chart[0].length && y >= 0 && y < chart.length;
-}
+type isOccupied = (x: number, y: number, chart: string[][]) => number;
 
-function totalOccupiedSeats(chart: string[][]): number {
-  return chart.flat(2).filter((ch) => ch == "#").length;
-}
-
-function slopeIsZero(xs: number, ys: number): boolean {
-  return xs == 0 && ys == 0;
+function flipSeats(chart: string[][], fn: isOccupied, tol: number): string[][] {
+  const newChart = cloneChart(chart);
+  for (let y = 0; y < chart.length; y++) {
+    for (let x = 0; x < chart[0].length; x++) {
+      if (chart[y][x] == "L" && fn(x, y, chart) == 0) {
+        newChart[y][x] = "#";
+      } else if (chart[y][x] == "#" && fn(x, y, chart) >= tol) {
+        newChart[y][x] = "L";
+      }
+    }
+  }
+  return newChart;
 }
 
 function adjOccupiedSeats(x: number, y: number, chart: string[][]): number {
@@ -74,49 +100,23 @@ function directionIsOccupied(
   return (directionIsOccupied(x + xs, y + ys, xs, ys, chart));
 }
 
-type isOccupied = (x: number, y: number, chart: string[][]) => number;
-
-function flipSeats(chart: string[][], fn: isOccupied, tol: number): string[][] {
-  const newChart = cloneChart(chart);
-  for (let y = 0; y < chart.length; y++) {
-    for (let x = 0; x < chart[0].length; x++) {
-      if (chart[y][x] == "L" && fn(x, y, chart) == 0) {
-        newChart[y][x] = "#";
-      } else if (chart[y][x] == "#" && fn(x, y, chart) >= tol) {
-        newChart[y][x] = "L";
-      }
-    }
-  }
-  return newChart;
+function seatingChart(): string[][] {
+  const input = Deno.readTextFileSync("../../input/2020/day11.txt");
+  return mod.chunked([...input].filter((c) => c != "\n"), input.indexOf("\n"));
 }
 
-function partOne(): void {
-  let beforeInput = seatingChart();
-
-  while (true) {
-    const afterInput = flipSeats(beforeInput, adjOccupiedSeats, 4);
-    if (equal(afterInput, beforeInput)) {
-      break;
-    }
-    beforeInput = cloneChart(afterInput);
-  }
-
-  console.log(totalOccupiedSeats(beforeInput));
+function cloneChart(chart: string[][]): string[][] {
+  return [...chart].map((line) => [...line]);
 }
 
-function partTwo(): void {
-  let beforeInput = seatingChart();
-
-  while (true) {
-    const afterInput = flipSeats(beforeInput, dirOccupiedSeats, 5);
-    if (equal(afterInput, beforeInput)) {
-      break;
-    }
-    beforeInput = cloneChart(afterInput);
-  }
-
-  console.log(totalOccupiedSeats(beforeInput));
+function indexInRange(x: number, y: number, chart: string[][]): boolean {
+  return x >= 0 && x < chart[0].length && y >= 0 && y < chart.length;
 }
 
-partOne();
-partTwo();
+function totalOccupiedSeats(chart: string[][]): number {
+  return chart.flat(2).filter((ch) => ch == "#").length;
+}
+
+function slopeIsZero(xs: number, ys: number): boolean {
+  return xs == 0 && ys == 0;
+}

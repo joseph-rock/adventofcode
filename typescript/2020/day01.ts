66c75e3ed8d06b6750ba596587069c20078e2320
Joseph <josephrock225@gmail.com>
2022-08-24 18:42:05 -0700

    Init

--- typescript/2020/day01.ts
+++ typescript/2020/day01.ts
@@ -9,4 +9,18 @@ function partOne() {
   console.log("Part 1: ", ans * (2020 - ans));
 }
 
+function partTwo() {
+  const input: number[] = puzzleInput();
+  const num1: number = input.find((e) => thirdNum(input, e)) || 0;
+  const num2: number = thirdNum(input, num1);
+  const num3: number = 2020 - num1 - num2;
+
+  console.log("Part 2: ", num1 * num2 * num3);
+}
+
+function thirdNum(input: number[], num: number): number {
+  return input.find((e) => input.includes(2020 - e - num)) || 0;
+}
+
 partOne();
+partTwo();

f9a6b5f2bcf52deb38177134ed4db027ce80fe4a
Joseph <josephrock225@gmail.com>
2022-08-26 21:28:09 -0700

    Finish pt1

--- typescript/2020/day12.ts
+++ typescript/2020/day12.ts
@@ -1,5 +1,4 @@
-import { equalSet } from "https://deno.land/x/equal@v1.4.0/equal.ts";
-import { map } from "https://deno.land/x/fonction@v1.6.2/mod.ts";
+import { input, print } from "../common.ts";
 
 interface instruction {
   dir: string;
@@ -7,7 +6,7 @@ interface instruction {
 }
 
 interface ship {
-  facing: string;
+  dir: string;
   n: number;
   s: number;
   e: number;
@@ -19,19 +18,36 @@ function changeFacing(inst: instruction, facing: string): string {
   const RIGHT = ["N", "E", "S", "W"];
 
   if (inst.dir == "L") {
-    return LEFT[LEFT.indexOf(facing) + (inst.amt / 90) % LEFT.length];
+    return LEFT[(LEFT.indexOf(facing) + (inst.amt / 90)) % LEFT.length];
   } else if (inst.dir == "R") {
-    return RIGHT[RIGHT.indexOf(facing) + (inst.amt / 90) % RIGHT.length];
+    return RIGHT[(RIGHT.indexOf(facing) + (inst.amt / 90)) % RIGHT.length];
   }
   return facing;
 }
 
-// TODO
 function moveForward(inst: instruction, ship: ship): ship {
+  const dir: string = ship.dir;
+
+  switch (dir) {
+    case "N":
+      ship.n += inst.amt;
+      break;
+    case "S":
+      ship.s += inst.amt;
+      break;
+    case "E":
+      ship.e += inst.amt;
+      break;
+    case "W":
+      ship.w += inst.amt;
+      break;
+    default:
+      break;
+  }
+
   return ship;
 }
 
-// TODO
 function executeInst(inst: instruction, ship: ship): ship {
   const dir: string = inst.dir;
 
@@ -50,7 +66,7 @@ function executeInst(inst: instruction, ship: ship): ship {
       break;
     case "L":
     case "R":
-      ship.facing = changeFacing(inst, ship.facing);
+      ship.dir = changeFacing(inst, ship.dir);
       break;
     case "F":
       ship = moveForward(inst, ship);
@@ -59,7 +75,6 @@ function executeInst(inst: instruction, ship: ship): ship {
       break;
   }
 
-  console.log(ship);
   return ship;
 }
 
@@ -68,16 +83,20 @@ function constructInst(inst: string): instruction {
 }
 
 function puzzleInput(): instruction[] {
-  const input = Deno.readTextFileSync("../../input/2020/day12.txt").split("\n");
-  return input.map((e) => constructInst(e));
+  return input(2020, 12)
+    .split("\n")
+    .map((e) => constructInst(e));
 }
 
 function partOne() {
-  let ship: ship = { facing: "E", n: 0, s: 0, e: 0, w: 0 };
+  const input = puzzleInput();
+  let ship: ship = { dir: "E", n: 0, s: 0, e: 0, w: 0 };
+
+  for (let i = 0; i < input.length; i++) {
+    ship = executeInst(input[i], ship);
+  }
+
+  return Math.abs(ship.e - ship.w) + Math.abs(ship.n - ship.s);
 }
 
-// const ship: ship = { facing: "E", n: 0, s: 0, e: 0, w: 0 };
-// console.log(changeFacing({ dir: "L", amt: 90 }, "N"));
-// console.log(executeInst({ dir: "L", amt: 90 }, ship));
-
-partOne();
+print(partOne());

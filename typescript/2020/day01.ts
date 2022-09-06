bda075c5ede3c9bafc49e5b8f7f3d1e04acfe893
Joseph <josephrock225@gmail.com>
2022-09-06 13:47:18 -0700

    Refactor

--- typescript/2020/day01.ts
+++ typescript/2020/day01.ts
@@ -1,3 +1,4 @@
+import { uniq } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 import { input, print } from "../common.ts";
 
 function puzzleInput(): number[] {
@@ -6,23 +7,14 @@ function puzzleInput(): number[] {
     .map((e) => parseInt(e));
 }
 
-function partOne(): number {
-  const input: number[] = puzzleInput();
-  const ans: number = input.find((e) => input.includes(2020 - e)) || 0;
-  return ans * (2020 - ans);
-}
+const partOne = (input: number[]) =>
+  input.flatMap((x) => input.flatMap((y) => x + y === 2020 ? [x * y] : []));
 
-function partTwo(): number {
-  const input: number[] = puzzleInput();
-  const num1: number = input.find((e) => thirdNum(input, e)) || 0;
-  const num2: number = thirdNum(input, num1);
-  const num3: number = 2020 - num1 - num2;
+const partTwo = (input: number[]) =>
+  input.flatMap((x) =>
+    input.flatMap((y) =>
+      input.flatMap((z) => x + y + z === 2020 ? [x * y * z] : [])
+    )
+  );
 
-  return num1 * num2 * num3;
-}
-
-function thirdNum(input: number[], num: number): number {
-  return input.find((e) => input.includes(2020 - e - num)) || 0;
-}
-
-print(partOne(), partTwo());
+print(uniq(partOne(puzzleInput())), uniq(partTwo(puzzleInput())));

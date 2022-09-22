70aed7bf565f3188c919d60c674ab35b9623fc91
Joseph <josephrock225@gmail.com>
2022-09-22 12:46:54 -0700

    Refactor

--- typescript/2020/day13.ts
+++ typescript/2020/day13.ts
@@ -1,4 +1,5 @@
 import { input, print } from "../common.ts";
+import { last } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 
 type Notes = {
   timestamp: number;
@@ -44,16 +45,17 @@ function makeBus(index: number, busID: number): Bus {
   return { index: index, busID: busID };
 }
 
-function isDivisable(target: number, bus: Bus): boolean {
+function isConcurrent(target: number, bus: Bus): boolean {
   return (target + bus.index) % bus.busID === 0;
 }
 
 function nextTimestamp(busList: Bus[], num: number): number {
+  const lastBus = last(busList);
   const increment = busList.reduce((total, bus) => total * bus.busID, 1) /
-    busList[busList.length - 1].busID;
+    lastBus.busID;
 
   while (num < Number.MAX_VALUE) {
-    if (busList.every((bus) => isDivisable(num, bus))) {
+    if (isConcurrent(num, lastBus)) {
       return num;
     }
     num += increment;
@@ -83,68 +85,3 @@ function partTwo(notes: Notes): number {
 }
 
 print(partOne(puzzleInput()), partTwo(puzzleInput()));
-
-/**
- * Pt 2 explination
- *
- * The input lists the buses at certain indicies, like below:
- *
- *    { index: 0,  busID: 19 }
- *    { index: 9,  busID: 41 }
- *    { index: 13, busID: 37 }
- *    { index: 19, busID: 367 }
- *    { index: 32, busID: 13 }
- *    { index: 36, busID: 17 }
- *    { index: 48, busID: 29 }
- *    { index: 50, busID: 373 }
- *    { index: 73, busID: 23 }
- *
- * We are trying to find a number, num, where each bus satisfies the following:
- *
- *    num + index % busID === 0
- *
- * If we start with a list of 1 bus
- *
- *    { index: 0,  busID: 19 }
- *
- * We can increment 0 by 1
- *
- *    0, 1, 2, ... 19
- *
- * Until we find 19
- *
- *    19 + 0 % 19 === 0
- *
- * Then we can add the second bus
- *
- *    { index: 0,  busID: 19 }
- *    { index: 9,  busID: 41 }
- *
- * Starting at 19, we increment by the first busID, or 19
- *
- *    19, 38, 57, ... 114
- *
- * Until we find 114, which satisfies our equation
- *
- *    114 + 0 % 19 === 0
- *    114 + 9 % 41 === 0
- *
- * We can now add the third bus
- *
- *    { index: 0,  busID: 19 }
- *    { index: 9,  busID: 41 }
- *    { index: 13, busID: 37 }
- *
- * Starting at 114, we increment by the first busID * second busID,
- *   or 19 * 41 === 779
- *
- *    114, 893, 1672 ... 22705
- *
- * Until we find 22705, which satisfies our equation
- *
- *    22705 + 0  % 19 === 0
- *    22705 + 9  % 41 === 0
- *    22705 + 13 % 37 === 0
- *
- * Continue this pattern!
- */

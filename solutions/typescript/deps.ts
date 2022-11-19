51cc99bdb384ce7755ac293576af679633c50879
Joseph <josephrock225@gmail.com>
2022-11-18 19:41:31 -0800

    Compressed imports

--- solutions/typescript/deps.ts
+++ solutions/typescript/deps.ts
@@ -1,3 +1,1 @@
-export { uniq } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
-export { equals } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
-export { last } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
+export { equals, last, uniq } from "https://deno.land/x/ramda@v0.27.2/mod.ts";

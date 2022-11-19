5d3d4aaa3b528deae552e76469560a0cdf7748af
Joseph <josephrock225@gmail.com>
2022-11-19 15:36:53 -0800

    New deps

--- solutions/typescript/deps.ts
+++ solutions/typescript/deps.ts
@@ -1,1 +1,7 @@
-export { equals, last, uniq } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
+export {
+  any,
+  equals,
+  last,
+  range,
+  uniq,
+} from "https://deno.land/x/ramda@v0.27.2/mod.ts";

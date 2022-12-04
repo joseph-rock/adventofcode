5cc420dd8849e46b67365095df4f8bf5c9e101b3
JRock <jrock@JRocks-MacBook-Pro.local>
2022-12-04 15:07:50 -0800

    Init

---
+++ solutions/typescript/2022/day03.ts
@@ -0,0 +1,61 @@
+import { input, print } from "../common.ts";
+import { range } from "../deps.ts";
+
+function priority(char: string): number {
+  const upper: string[] = range(65, 91)
+    .map((x: number) => String.fromCharCode(x));
+  const lower: string[] = range(97, 123)
+    .map((x: number) => String.fromCharCode(x));
+  const all = lower.concat(upper);
+  return all.indexOf(char) + 1;
+}
+
+function common(a: string, b: string) {
+  const c = new Set<string>();
+  const compare = [...b];
+
+  for (const char of [...a]) {
+    if (compare.includes(char)) {
+      c.add(char);
+    }
+  }
+  return Array.from(c);
+}
+
+function commonThree(a: string, b: string, c: string): string {
+  return common(common(a, b).join(""), c).toString();
+}
+
+function pt1(raw: string) {
+  const supplies = raw.split("\n");
+  const commonList: string[] = [];
+
+  for (const bag of supplies) {
+    const mid = bag.length / 2;
+    const compA = bag.slice(0, mid);
+    const compB = bag.slice(mid);
+    commonList.push(...common(compA, compB));
+  }
+
+  return commonList.reduce((total, char) => total += priority(char), 0);
+}
+
+function pt2(raw: string) {
+  const supplies = raw.split("\n");
+  const badgeList: string[] = [];
+  let group: string[] = [];
+
+  for (const bag of supplies) {
+    if (group.length === 3) {
+      badgeList.push(commonThree(group[0], group[1], group[2]));
+      group = [];
+    }
+    group.push(bag);
+  }
+
+  badgeList.push(commonThree(group[0], group[1], group[2]));
+  return badgeList.reduce((total, char) => total += priority(char), 0);
+}
+
+const raw = input(2022, 3);
+print(pt1(raw), pt2(raw));

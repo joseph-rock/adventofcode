1a6e166a1c8166583199e0429915d2eac78511e9
Joseph <josephrock225@gmail.com>
2022-12-05 12:12:51 -0800

    Refactor

--- solutions/typescript/2022/day04.ts
+++ solutions/typescript/2022/day04.ts
@@ -5,41 +5,39 @@ interface Range {
   max: number;
 }
 
-function formatRange(raw: string) {
+function ranges(raw: string) {
   const value = raw.split(/[-|,]/).map((num) => parseInt(num));
   return [{ min: value[0], max: value[1] }, { min: value[2], max: value[3] }];
 }
 
-function fullContain(l: Range, r: Range): boolean {
-  return (l.min <= r.min && l.max >= r.max) ||
-    (r.min <= l.min && r.max >= l.max);
+function fullContain(a: Range, b: Range): boolean {
+  return (a.min <= b.min && a.max >= b.max) ||
+    (b.min <= a.min && b.max >= a.max);
 }
 
-function overlap(l: Range, r: Range): boolean {
-  let low = l;
-  let high = r;
-  if (l.min > r.min) {
-    low = r;
-    high = l;
+function overlap(a: Range, b: Range): boolean {
+  let low = a;
+  let high = b;
+  if (a.min > b.min) {
+    low = b;
+    high = a;
   }
 
   return low.max >= high.min;
 }
 
 function pt1(raw: string) {
-  const sections = raw.split("\n");
-  const ranges = sections.map((group) => formatRange(group));
-  return ranges.reduce(
-    (sum, group) => fullContain(group[0], group[1]) ? sum + 1 : sum,
+  const elfPairs = raw.split("\n").map((group) => ranges(group));
+  return elfPairs.reduce(
+    (sum, elf) => fullContain(elf[0], elf[1]) ? sum + 1 : sum,
     0,
   );
 }
 
 function pt2(raw: string) {
-  const sections = raw.split("\n");
-  const ranges = sections.map((group) => formatRange(group));
-  return ranges.reduce(
-    (sum, group) => overlap(group[0], group[1]) ? sum + 1 : sum,
+  const elfPairs = raw.split("\n").map((group) => ranges(group));
+  return elfPairs.reduce(
+    (sum, elf) => overlap(elf[0], elf[1]) ? sum + 1 : sum,
     0,
   );
 }

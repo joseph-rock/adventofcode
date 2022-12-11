5b8e516ee48df98f96872bd67b9948b61c564ddd
Joseph <josephrock225@gmail.com>
2022-12-11 01:03:38 -0800

    Refactor

--- solutions/typescript/2022/day09.ts
+++ solutions/typescript/2022/day09.ts
@@ -1,4 +1,5 @@
 import { input, print } from "common";
+import { last } from "ramda";
 
 interface POS {
   x: number;
@@ -10,7 +11,16 @@ interface Instruction {
   amt: number;
 }
 
-function POSIdx(pos: POS): string {
+function instructions(raw: string): Instruction[] {
+  return raw
+    .split("\n")
+    .map((line) => {
+      const l = line.split(" ");
+      return { dir: l[0], amt: parseInt(l[1]) };
+    });
+}
+
+function idx(pos: POS): string {
   return pos.x.toString() + "," + pos.y.toString();
 }
 
@@ -19,21 +29,6 @@ function inRange(head: POS, tail: POS): boolean {
   const col = [head.y - 1, head.y, head.y + 1];
 
   return row.includes(tail.x) && col.includes(tail.y);
-}
-
-function moveTail(head: POS, tail: POS): POS {
-  const next = { x: tail.x, y: tail.y };
-  if (inRange(head, tail)) return next;
-  if (head.x === tail.x) {
-    next.y += (head.y - tail.y) / Math.abs(head.y - tail.y);
-  } else if (head.y === tail.y) {
-    next.x += (head.x - tail.x) / Math.abs(head.x - tail.x);
-  } else {
-    next.y += (head.y - tail.y) / Math.abs(head.y - tail.y);
-    next.x += (head.x - tail.x) / Math.abs(head.x - tail.x);
-  }
-
-  return next;
 }
 
 function moveHead(head: POS, inst: Instruction): POS {
@@ -57,52 +52,56 @@ function moveHead(head: POS, inst: Instruction): POS {
   return next;
 }
 
+function moveTail(head: POS, tail: POS): POS {
+  if (inRange(head, tail)) return tail;
+
+  const next = { x: tail.x, y: tail.y };
+  const xdiff = (head.x - tail.x) / Math.abs(head.x - tail.x);
+  const ydiff = (head.y - tail.y) / Math.abs(head.y - tail.y);
+
+  if (head.x === tail.x) {
+    next.y += ydiff;
+  } else if (head.y === tail.y) {
+    next.x += xdiff;
+  } else {
+    next.y += ydiff;
+    next.x += xdiff;
+  }
+
+  return next;
+}
+
 function pt1(raw: string) {
-  const instructions = raw.split("\n").map((line) => {
-    const l = line.split(" ");
-    return { dir: l[0], amt: parseInt(l[1]) };
-  });
+  const instList = instructions(raw);
 
   let head: POS = { x: 0, y: 0 };
   let tail: POS = { x: 0, y: 0 };
-
   const visited: Record<string, POS> = {};
-  visited[POSIdx(tail)] = tail;
 
-  for (const inst of instructions) {
+  for (const inst of instList) {
     for (let i = 0; i < inst.amt; i++) {
       head = moveHead(head, inst);
       tail = moveTail(head, tail);
-      visited[POSIdx(tail)] = tail;
+      visited[idx(tail)] = tail;
     }
   }
   return Object.keys(visited).length;
 }
 
 function pt2(raw: string) {
-  const instructions = raw.split("\n").map((line) => {
-    const l = line.split(" ");
-    return { dir: l[0], amt: parseInt(l[1]) };
-  });
+  const instList = instructions(raw);
 
-  let head: POS = { x: 0, y: 0 };
-  const tail: POS[] = [];
-  for (let i = 0; i < 9; i++) {
-    tail.push({ x: 0, y: 0 });
-  }
-
+  const knots: POS[] = Array(10).fill({ x: 0, y: 0 });
   const visited: Record<string, POS> = {};
-  visited[POSIdx(tail[8])] = tail[8];
 
-  for (const inst of instructions) {
+  for (const inst of instList) {
     for (let i = 0; i < inst.amt; i++) {
-      head = moveHead(head, inst);
-      tail.forEach((_, i) =>
+      knots.forEach((_, i) =>
         i === 0
-          ? tail[i] = moveTail(head, tail[i])
-          : tail[i] = moveTail(tail[i - 1], tail[i])
+          ? knots[i] = moveHead(knots[0], inst)
+          : knots[i] = moveTail(knots[i - 1], knots[i])
       );
-      visited[POSIdx(tail[8])] = tail[8];
+      visited[idx(last(knots))] = last(knots);
     }
   }
   return Object.keys(visited).length;

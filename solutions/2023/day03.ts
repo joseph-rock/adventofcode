a6215652f618fa5a999114fbb3dd801a8e679a7f
joseph-rock <josephrock225@gmail.com>
2023-12-11 19:20:52 -0800

    Init pt2

--- solutions/2023/day03.ts
+++ solutions/2023/day03.ts
@@ -2,7 +2,7 @@ import { input, print } from "common";
 
 function main() {
   const raw = input(2023, 3);
-  print(pt1(raw));
+  print(pt1(raw), pt2(raw));
 }
 
 function pt1(raw: string): number {
@@ -19,6 +19,24 @@ function pt1(raw: string): number {
   }
   return partNumbers
     .reduce((total, partNumber) => total += partNumber, 0);
+}
+
+function pt2(raw: string): number {
+  const schematic = engineSchematic(raw);
+  const gearRatios = [];
+  for (let y = 0; y < schematic.length; y++) {
+    for (let x = 0; x < schematic[0].length; x++) {
+      if (schematic[y][x] === "*") {
+        const partNumbers = adjacentNumbers(schematic, y, x);
+        if (partNumbers.length === 2) {
+          const gearRatio = partNumbers[0] * partNumbers[1];
+          gearRatios.push(gearRatio);
+        }
+      }
+    }
+  }
+  return gearRatios
+    .reduce((total, ratio) => total += ratio, 0);
 }
 
 function engineSchematic(raw: string): string[][] {
@@ -46,7 +64,32 @@ function hasAdjacentSymbol(
       }
     }
   }
+
   return false;
+}
+
+function adjacentNumbers(
+  schematic: string[][],
+  yCoord: number,
+  xCoord: number,
+): number[] {
+  const xLimit = schematic[0].length - 1;
+  const yLimit = schematic.length - 1;
+  const partNumbers = [];
+
+  for (let y = yCoord - 1; y <= yCoord + 1; y++) {
+    if (y < 0 || y > yLimit) continue;
+    for (let x = xCoord - 1; x <= xCoord + 1; x++) {
+      if (x < 0 || x > xLimit) continue;
+      if (isNum(schematic[y][x])) {
+        x = rightBoundary(schematic[y], x);
+        const partNumber = getPartNumber(schematic[y], x);
+        partNumbers.push(partNumber);
+      }
+    }
+  }
+
+  return partNumbers;
 }
 
 function getPartNumber(line: string[], cursor: number): number {

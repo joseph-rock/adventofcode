bba52cd90273defbe8fe753c0a3f1dda06cee778
joseph-rock <josephrock225@gmail.com>
2023-12-11 19:07:09 -0800

    Init pt 1

--- solutions/2023/day03.ts
+++ solutions/2023/day03.ts
@@ -2,34 +2,44 @@ import { input, print } from "common";
 
 function main() {
   const raw = input(2023, 3);
-  pt1(raw);
+  print(pt1(raw));
 }
 
-function pt1(raw: string) {
+function pt1(raw: string): number {
   const schematic = engineSchematic(raw);
+  const partNumbers = [];
   for (let y = 0; y < schematic.length; y++) {
     for (let x = 0; x < schematic[0].length; x++) {
-      if (isNum(schematic[y][x]) && checkAdjacent(schematic, y, x)) {
-        console.log(x, y);
+      if (isNum(schematic[y][x]) && hasAdjacentSymbol(schematic, y, x)) {
+        x = rightBoundary(schematic[y], x);
+        const partNumber = getPartNumber(schematic[y], x);
+        partNumbers.push(partNumber);
       }
     }
   }
-}
-
-function pt2(raw: string) {
+  return partNumbers
+    .reduce((total, partNumber) => total += partNumber, 0);
 }
 
 function engineSchematic(raw: string): string[][] {
   return raw.split("\n").map((line) => line.split(""));
 }
 
-function checkAdjacent(schematic: string[][], ycoord: number, xcoord: number) {
+function isNum(string: string): boolean {
+  return /^[0-9]*$/.test(string);
+}
+
+function hasAdjacentSymbol(
+  schematic: string[][],
+  yCoord: number,
+  xCoord: number,
+): boolean {
   const xLimit = schematic[0].length - 1;
   const yLimit = schematic.length - 1;
 
-  for (let y = ycoord - 1; y <= ycoord + 1; y++) {
+  for (let y = yCoord - 1; y <= yCoord + 1; y++) {
     if (y < 0 || y > yLimit) continue;
-    for (let x = xcoord - 1; x <= xcoord + 1; x++) {
+    for (let x = xCoord - 1; x <= xCoord + 1; x++) {
       if (x < 0 || x > xLimit) continue;
       if (schematic[y][x] !== "." && !isNum(schematic[y][x])) {
         return true;
@@ -39,8 +49,21 @@ function checkAdjacent(schematic: string[][], ycoord: number, xcoord: number) {
   return false;
 }
 
-function isNum(string: string): boolean {
-  return /^[0-9]*$/.test(string);
+function getPartNumber(line: string[], cursor: number): number {
+  const left = leftBoundary(line, cursor);
+  const right = rightBoundary(line, cursor) + 1;
+  const num = line.slice(left, right).join("");
+  return parseInt(num);
+}
+
+function rightBoundary(line: string[], x: number): number {
+  if (x >= line.length || !isNum(line[x + 1])) return x;
+  return rightBoundary(line, x + 1);
+}
+
+function leftBoundary(line: string[], x: number): number {
+  if (x < 0 || !isNum(line[x - 1])) return x;
+  return leftBoundary(line, x - 1);
 }
 
 main();

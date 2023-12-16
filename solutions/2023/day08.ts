3f8dd844b72c1ad6b7ec9bd5991b7dd807e046a4
joseph-rock <josephrock225@gmail.com>
2023-12-16 15:23:43 -0800

    Unfinished

--- solutions/2023/day08.ts
+++ solutions/2023/day08.ts
@@ -1,4 +1,6 @@
 import { input, print } from "common";
+import { MissingCommandNameCompletionsError } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/_errors.ts";
+import { mapAccum, mapObjIndexed } from "ramda";
 
 type Map = {
   instructions: string[];
@@ -13,22 +15,37 @@ type Node = {
 
 function main() {
   const raw = input(2023, 8);
-  print(pt1(raw));
+  print(pt1(raw), pt2(raw));
 }
 
 function pt1(raw: string): number {
   const map = parseInstructions(raw);
+  return count(map, map.nodes["AAA"], map.nodes["ZZZ"]);
+}
 
+function pt2(raw: string): number {
+  const map = parseInstructions(raw);
+
+  const nodes = Object.keys(map.nodes)
+    .filter((id) => id.charAt(2) === "A")
+    .map((id) => map.nodes[id])
+    .map((node) => countToZ(map, node));
+
+  // find LCM of steps and return
+
+  const factors = nodes.map((node) => primeFactors(node.count)).flat();
+  console.log(factors);
+
+  return nodes[5].count;
+}
+
+function count(map: Map, start: Node, end: Node): number {
   let count = 0;
   let index = 0;
-  let curr = map.nodes["AAA"];
+  let node = start;
 
-  while (curr.id !== "ZZZ") {
-    if (map.instructions[index] === "L") {
-      curr = map.nodes[curr.left];
-    } else {
-      curr = map.nodes[curr.right];
-    }
+  while (node !== end) {
+    node = next(map, node, map.instructions[index]);
     count += 1;
     index = count % map.instructions.length;
   }
@@ -36,17 +53,33 @@ function pt1(raw: string): number {
   return count;
 }
 
+function countToZ(map: Map, start: Node) {
+  let count = 1;
+  let index = 1;
+  let node = next(map, start, map.instructions[0]);
+
+  while (node.id.charAt(2) !== "Z") {
+    node = next(map, node, map.instructions[index]);
+    count += 1;
+    index = count % map.instructions.length;
+  }
+  return { node: node, count: count };
+}
+
+function next(map: Map, node: Node, dir: string): Node {
+  if (dir === "L") {
+    return map.nodes[node.left];
+  }
+  return map.nodes[node.right];
+}
+
 function parseInstructions(raw: string): Map {
   const parts = raw.split("\n\n");
   const instructions = parts[0].split("");
-  const lines = parts[1]
+  const nodes = parts[1]
     .split("\n")
-    .map((line) => parseNode(line));
-
-  const nodes: Record<string, Node> = {};
-  for (const line of lines) {
-    nodes[line.id] = line;
-  }
+    .map((line) => parseNode(line))
+    .reduce((nodes, node) => ({ ...nodes, [node.id]: node }), {});
 
   return {
     instructions: instructions,
@@ -63,6 +96,28 @@ function parseNode(line: string): Node {
     left: match!.groups!.left,
     right: match!.groups!.right,
   };
+}
+
+function primeFactor(num: number): number {
+  const truncatedNum: number = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= truncatedNum; i++) {
+    if (num % i == 0) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+function primeFactors(num: number): number[] {
+  const factors: number[] = [];
+
+  while (primeFactor(num) > 0) {
+    factors.push(primeFactor(num));
+    num = num / primeFactor(num);
+  }
+  factors.push(num);
+
+  return factors;
 }
 
 main();

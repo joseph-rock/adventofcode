6fcdde5f29dc655e7ac2af7bde8c199f89be538a
Joseph Rock <josephrock225@gmail.com>
2023-12-20 16:16:29 -0800

    Init

---
+++ solutions/2023/day11.ts
@@ -0,0 +1,27 @@
+import { input, print } from "common";
+
+function main() {
+  const raw = input(2023, 11);
+  pt1(raw);
+}
+
+function pt1(raw: string) {
+  const space = raw.split("\n")
+    .map((line) => line.split(""));
+  expandSpace(space);
+}
+
+function expandSpace(space: string[][]): string[][] {
+  for (let i = 0; i < space.length; i++) {
+    if (space[i].every((char) => char === ".")) {
+      space.splice(i, 0, space[i]);
+      i += 1;
+    }
+  }
+
+  // TODO: expand columns
+
+  return space;
+}
+
+main();

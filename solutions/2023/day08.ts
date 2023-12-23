b2ab2bcfcd45d5d4c5cbec5f53251dee307db032
joseph-rock <josephrock225@gmail.com>
2023-12-23 09:17:28 -0800

    Refactor step counting

--- solutions/2023/day08.ts
+++ solutions/2023/day08.ts
@@ -1,7 +1,7 @@
 import { input, leastCommonMultiple, print } from "common";
 
 type Network = {
-  instructions: string[];
+  inst: string[];
   nodes: Record<string, Node>;
 };
 
@@ -18,28 +18,31 @@ function main() {
 
 function pt1(raw: string): number {
   const network = parseNetwork(raw);
-  return countToZZZ(network, network.nodes["AAA"]);
+  return stepsToNode(network, network.nodes["AAA"], ["ZZZ"]);
 }
 
 function pt2(raw: string): number {
   const network = parseNetwork(raw);
-  const stepCounts = Object.keys(network.nodes)
-    .filter((id) => id.charAt(2) === "A")
-    .map((id) => countToAnyZ(network, network.nodes[id]));
+  const startIds = Object.keys(network.nodes)
+    .filter((id) => id.charAt(2) === "A");
+  const endIds = Object.keys(network.nodes)
+    .filter((id) => id.charAt(2) === "Z");
+  const stepCounts = startIds
+    .map((id) => stepsToNode(network, network.nodes[id], endIds));
   return leastCommonMultiple(stepCounts);
 }
 
 function parseNetwork(raw: string): Network {
   const parts = raw.split("\n\n");
-  const instructions = parts[0].split("");
+  const inst = parts[0].split("");
   const nodes = parts[1]
     .split("\n")
     .map((line) => parseNode(line))
     .reduce((nodes, node) => ({ ...nodes, [node.id]: node }), {});
 
   return {
-    instructions: instructions,
-    nodes: nodes,
+    inst,
+    nodes,
   };
 }
 
@@ -54,41 +57,23 @@ function parseNode(line: string): Node {
   };
 }
 
-function countToZZZ(map: Network, start: Node): number {
+function stepsToNode(
+  map: Network,
+  start: Node,
+  end: string[],
+): number {
   let count = 0;
   let index = 0;
   let node = start;
 
-  while (node.id !== "ZZZ") {
-    const nextID = next(node, map.instructions[index]);
+  while (!end.includes(node.id)) {
+    const nextID = map.inst[index] === "L" ? node.left : node.right;
     node = map.nodes[nextID];
     count += 1;
-    index = count % map.instructions.length;
+    index = count % map.inst.length;
   }
 
   return count;
-}
-
-function countToAnyZ(map: Network, start: Node): number {
-  let count = 0;
-  let index = 0;
-  let node = start;
-
-  while (node.id.charAt(2) !== "Z") {
-    const nextID = next(node, map.instructions[index]);
-    node = map.nodes[nextID];
-    count += 1;
-    index = count % map.instructions.length;
-  }
-
-  return count;
-}
-
-function next(node: Node, dir: string): string {
-  if (dir === "L") {
-    return node.left;
-  }
-  return node.right;
 }
 
 main();
